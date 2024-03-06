6d18b3d14e678a5061b9261545d2644e19e837b7
seolhwayam <susung462@gmail.com>
2024-03-06 14:32:23 +0900

    "CalculatorMethod 수정"

--- DIGI_CAMPUS/src/CalculatorMethod.java
+++ DIGI_CAMPUS/src/CalculatorMethod.java
@@ -4,24 +4,16 @@ public class CalculatorMethod {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		CalculatorMethod.output(CalculatorMethod.operator(),CalculatorMethod.inputNum1(),CalculatorMethod.inputNum2());
+		Scanner sc= new Scanner(System.in);
+		CalculatorMethod.output(CalculatorMethod.operator(sc),CalculatorMethod.inputNum(sc),CalculatorMethod.inputNum(sc));
 
 	}
-	public static int inputNum1() { // 첫번째 숫자를 입력받는 함수
-		Scanner sc= new Scanner(System.in);
-		System.out.println("첫 번째 숫자를 입력해주세요 : ");
+	public static int inputNum(Scanner sc) { // 첫번째 숫자를 입력받는 함수
+		System.out.println("숫자를 입력해주세요 : ");
 		int num1 = sc.nextInt();
 		return num1;
 	}
-	public static int inputNum2() {// 두번째 숫자를 입력받는 함수
-		Scanner sc= new Scanner(System.in);
-		System.out.println("두 번째 숫자를 입력해주세요 : ");
-		int num2 = sc.nextInt();
-		return num2;
-	}
-	public static String operator() { // 연산자를 입력받는 함수
-		Scanner sc= new Scanner(System.in);
+	public static String operator(Scanner sc) { // 연산자를 입력받는 함수
 		System.out.println("연산자를 입력해주세요(+,-,*,/) : ");
 		String operator = sc.next();
 		return operator;
@@ -29,6 +21,7 @@ public class CalculatorMethod {
 	
 	
 	public static void output(String operator,int num1, int num2) { //연산자 구분해서 출력하기
+		int result=0;
 		switch(operator) {
 			case "+" :
 				System.out.println(num1+operator+num2+"="+(CalculatorMethod.puls(num1,num2)));
