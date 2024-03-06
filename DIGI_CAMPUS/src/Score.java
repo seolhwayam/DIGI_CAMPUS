6dc66db317d5d4928ec71d11e7b6996a699a7181
seolhwayam <susung462@gmail.com>
2024-03-06 16:25:12 +0900

    "배열 예제"

---
+++ DIGI_CAMPUS/src/Score.java
@@ -0,0 +1,45 @@
+
+/*시험의 성적처리 프로그램
+ * 입력 : 이름,국,영,수
+ * 연산 : 총점,평균
+ * 출력 : 이름,국,영,수,총,평
+ * */
+
+import java.util.Scanner;
+
+public class Score {
+	public static void main(String[] args) {
+		String name;
+		int score[] = new int[4]; //국,영,수,총
+		float avg;
+		
+		//이름 입력
+		Scanner sc = new Scanner(System.in);
+		System.out.println("이름를 입력해 주세요");
+		name = sc.next();
+		//점수 입력
+		for(int i=0; i<(score.length)-1; i++) {
+			System.out.println("국,영,수 순서대로 점수를 입력해 주세요");
+			score[i] = sc.nextInt();
+			score[3]+=score[i];
+		}
+		
+		배열[0]= 1;
+		//평균 계산
+		avg=score[3]/3;
+		//출력
+		System.out.print(name+",");
+		for(int i:score) {
+			System.out.print(i+",");
+		}
+		System.out.print(avg);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
