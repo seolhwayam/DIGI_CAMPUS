209215c98793f0cb8e4c3da94462ca63e33dc608
seolhwayam <susung462@gmail.com>
2024-03-06 16:24:33 +0900

    "Array 공부"

---
+++ DIGI_CAMPUS/src/ArrayExam.java
@@ -0,0 +1,13 @@
+
+public class ArrayExam {
+	public static void main(String[] args) {
+		int []arr = new int[50];
+		
+		for(int i=0; i<arr.length; i++) { //arr.length : 배열 갯수
+			System.out.println(arr[i]); //모두 0이 나옴, 초기화 안할 시 자동으로 0 초기화
+		}
+
+		
+	}
+
+}
