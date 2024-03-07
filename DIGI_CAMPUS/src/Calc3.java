9e8fb838db56909de96cd575806a6c1575a05d93
seolhwayam <susung462@gmail.com>
2024-03-07 15:08:57 +0900

    "클래스 계산기"

---
+++ DIGI_CAMPUS/src/Calc3.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+
+public class Calc3 {
+	//변수선언
+	private	int num1,num2;
+	private	char ch;
+	private double result; 
+			
+	public Calc3() {
+		
+	}
+	public Calc3(int n1,char c,int n2) {
+		num1 = n1;
+		ch = c;
+		num2 = n2; 
+	}
+	
+	public void setNum1(int n) {
+		num1 = n;
+	}
+	public void setNum2(int n) {
+		num2 = n;
+	}
+	public void setCh(char c) {
+		ch = c; 
+	}
+	
+	public int getNum1() {
+		return num1; 
+	}
+	public int getNum2() {
+		return num2;
+	}
+	public char getCh() {
+		return ch;
+	}
+	
+	public double puls(int num1, int num2) { // 더하기 연산 함수
+		return result = num1+num2;
+	}
+	public double minus(int num1, int num2) { // 빼기 연산 함수
+		return result = num1-num2;
+	}
+	public double multiply(int num1, int num2) { // 곱하기 연산 함수
+		return result = num1*num2;
+	}
+	public double divide(int num1, int num2) { // 나누기 연산 함수
+		return result = num1/num2;
+	}
+	
+	public void outPut(int n1,int n2) {
+		System.out.println(num1+""+ch+""+num2+"="+result);
+	}
+	
+	
+	public void select() {
+		switch (ch) {
+			case '+':
+				puls(num1,num2);
+				break;
+			case '-':
+				minus(num1,num2);
+				break;
+			case '*':
+				multiply(num1,num2);
+				break;
+			case '/':
+				divide(num1,num2);
+				break;
+		}
+		outPut(num1,num2);
+	}
+			
+				
+
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		char ch = sc.next().charAt(0);
+		int num2 = sc.nextInt();
+		
+		Calc3 calc = new Calc3(num,ch,num2);
+		calc.select();
+	
+	}
+
+}
+
