3158aae6f69f1afc5a3998805142e1c35c04b091
seolhwayam <susung462@gmail.com>
2024-03-08 14:33:53 +0900

    "클래스 가져와서 이용하기"

---
+++ DIGI_CAMPUS/src/Subject.java
@@ -0,0 +1,18 @@
+
+public class Subject {
+	private int Score;
+
+	public Subject() {
+		
+	}
+
+	public int getScore() {
+		return Score;
+	}
+
+	public void setScore(int score) {
+		Score = score;
+	}
+	
+
+}
