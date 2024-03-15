a983fb3062fca24825f459e69dae184163a6f12b
seolhwayam <susung462@gmail.com>
2024-03-15 10:19:01 +0900

    "프레임 상속버전"

---
+++ DIGI_CAMPUS/src/Day10/FrameEx02.java
@@ -0,0 +1,17 @@
+package Day10;
+
+import java.awt.Frame;
+
+public class FrameEx02 extends Frame{
+	public FrameEx02() {
+		super("Frame Test");
+		
+		setSize(300,300);
+		setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) {
+		FrameEx02 fe = new FrameEx02();
+	}
+}
