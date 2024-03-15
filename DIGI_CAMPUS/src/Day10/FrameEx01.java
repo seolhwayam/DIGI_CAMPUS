ddd163cda55558590de251ff75f38edc373934d6
seolhwayam <susung462@gmail.com>
2024-03-15 10:18:51 +0900

    "프레임 IMPORT버전"

---
+++ DIGI_CAMPUS/src/Day10/FrameEx01.java
@@ -0,0 +1,15 @@
+package Day10;
+
+import java.awt.Frame;
+
+public class FrameEx01 {
+	public static void main(String[] args) {
+		Frame f =new Frame();
+		
+		f.setTitle("Frame Test");
+		
+		f.setSize(300,300);
+		f.setVisible(true);
+	}
+
+}
