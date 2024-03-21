fa838f79d37d5f4b9e8a699a3e1df074c637345b
seolhwayam <susung462@gmail.com>
2024-03-21 10:39:48 +0900

    "팩토리얼 반복문 버전"

--- DIGI_CAMPUS/src/Day14/Factorial.java
+++ DIGI_CAMPUS/src/Day14/Factorial.java
@@ -7,11 +7,12 @@ public class Factorial {
 		System.out.println("팩토리얼 수를 입력해주세요 : ");
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
+		int check = num;
 		for(int i=num-1; i>0; i--) {
 			num*=i;
 		}
 		
-		for(int i=5; i>0; i--) {
+		for(int i=check; i>0; i--) {
 			if(i!=1) {
 				System.out.print(i+"*");
 			}else {
